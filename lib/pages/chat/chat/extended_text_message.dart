25d03bf46e445c8905e557e6866ac28ce2d2d652
hujs888 <hujs@curltech.io>
2022-08-07 03:40:49 +0800

    message widget

--- lib/pages/chat/chat/extended_text_message.dart
+++ lib/pages/chat/chat/extended_text_message.dart
@@ -1,13 +1,16 @@
 import 'package:extended_text/extended_text.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../../../widgets/special_text/custom_special_text_span_builder.dart';
 
 ///消息体：扩展文本
 class ExtendedTextMessage extends StatelessWidget {
   final String content;
   final bool isMyself;
+  final CustomSpecialTextSpanBuilder customSpecialTextSpanBuilder =
+      CustomSpecialTextSpanBuilder();
 
-  const ExtendedTextMessage(
-      {Key? key, required this.content, required this.isMyself})
+  ExtendedTextMessage({Key? key, required this.content, required this.isMyself})
       : super(key: key);
 
   @override
@@ -18,6 +21,18 @@ class ExtendedTextMessage extends StatelessWidget {
         color: isMyself ? Colors.white : Colors.black,
         //fontSize: 16.0,
       ),
+      specialTextSpanBuilder: customSpecialTextSpanBuilder,
+      onSpecialTextTap: (dynamic value) {
+        if (value.toString().startsWith('\$')) {
+          launchUrl(
+              Uri(scheme: 'https', host: 'github.com', path: 'fluttercandies'));
+        } else if (value.toString().startsWith('@')) {
+          launchUrl(Uri(
+            scheme: 'mailto',
+            path: 'zmtzawqlp@live.com',
+          ));
+        }
+      },
     );
   }
 }
