2e181b888f471cfb74fd7ee35280b69af963f846
hujs888 <hujs@curltech.io>
2022-08-11 23:25:33 +0800

    message widget

--- lib/transport/webrtc/peer_connection_pool.dart
+++ lib/transport/webrtc/peer_connection_pool.dart
@@ -501,7 +501,7 @@ class PeerConnectionPool {
   /// @param data
   Future<void> send(String peerId, List<int> data,
       {String? clientId,
-      CryptoOption cryptoOption = CryptoOption.cryptography}) async {
+      CryptoOption cryptoOption = CryptoOption.signal}) async {
     List<AdvancedPeerConnection>? peerConnections = get(peerId);
     if (peerConnections != null && peerConnections.isNotEmpty) {
       List<Future<void>> ps = [];
