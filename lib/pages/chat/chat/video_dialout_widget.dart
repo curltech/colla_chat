160e5335536e18c877b1f13d257f6314c6ce0552
hujs888 <hujs@curltech.io>
2022-08-19 06:23:15 +0800

    video chat

--- lib/pages/chat/chat/video_dialout_widget.dart
+++ lib/pages/chat/chat/video_dialout_widget.dart
@@ -14,7 +14,17 @@ import '../../../widgets/common/image_widget.dart';
 import '../../../widgets/common/widget_mixin.dart';
 import 'controller/local_media_controller.dart';
 
-///视频通话拨出的对话框
+///视频通话的流程
+///1.发起方发起视频通话请求，激活拨出窗口；
+///2.接收方接收视频通话请求，激活拨入对话框；
+///3.接收方选择接受或者拒绝，如果接受，发送回执，关闭对话框，激活本地视频并加入连接，打开通话窗口
+///4.接收方选择拒绝，发送回执，关闭对话框
+///5.发起方收到回执，如果是接受回执，关闭拨出窗口，激活本地视频并加入连接，打开通话窗口，等待远程视频流到来，显示
+///6.发起方收到回执，如果是拒绝回执，关闭拨出窗口
+///7.接收方等待远程视频流到来，显示
+///8.如果发起方在接收回执到来前，自己主动终止请求，执行挂断操作，设置挂断标志，对远程流不予接受
+
+///视频通话拨出的窗口
 class VideoDialOutWidget extends StatefulWidget with TileDataMixin {
   VideoDialOutWidget({
     Key? key,
