5201ed3ad91b82af33dbae7c0bc2e94f43b95993
hujs888 <hujs@curltech.io>
2022-08-30 06:46:12 +0800

    video chat

--- lib/transport/webrtc/base_peer_connection.dart
+++ lib/transport/webrtc/base_peer_connection.dart
@@ -451,11 +451,11 @@ class BasePeerConnection {
 
   ///onIceCandidate事件表示本地candidate准备好，可以发送IceCandidate到远端
   onIceCandidate(RTCIceCandidate candidate) {
-    var handler = handlers[WebrtcEventType.iceCandidate];
-    if (handler != null) {
-      handler(candidate);
-      return;
-    }
+    // var handler = handlers[WebrtcEventType.iceCandidate];
+    // if (handler != null) {
+    //   handler(candidate);
+    //   return;
+    // }
     localCandidate = candidate;
     //logger.i('onIceCandidate event:${candidate.toMap()}');
     if (status == PeerConnectionStatus.closed) {
@@ -1058,11 +1058,11 @@ class BasePeerConnection {
   ///对远端的连接来说，当有stream或者track到来时触发
   ///什么都不做，由onAddTrack事件处理
   onAddStream(MediaStream stream) {
-    var handler = handlers[WebrtcEventType.stream];
-    if (handler != null) {
-      handler(stream);
-      return;
-    }
+    // var handler = handlers[WebrtcEventType.stream];
+    // if (handler != null) {
+    //   handler(stream);
+    //   return;
+    // }
     logger.i('onAddStream stream:${stream.id}');
     _addStream(stream);
     emit(WebrtcEventType.stream, stream);
