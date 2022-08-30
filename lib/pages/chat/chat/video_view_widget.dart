913bcfeac554e8f7f96bc056b0d25d5b50c374a1
hujs888 <hujs@curltech.io>
2022-08-30 19:28:58 +0800

    video chat

--- lib/pages/chat/chat/video_view_widget.dart
+++ lib/pages/chat/chat/video_view_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../transport/webrtc/peer_video_render.dart';
+import 'controller/peer_connections_controller.dart';
 
 ///单个视频窗口
 class VideoViewWidget extends StatefulWidget {
@@ -20,6 +21,11 @@ class _VideoViewWidgetState extends State<VideoViewWidget> {
   @override
   initState() {
     super.initState();
+    peerConnectionsController.addListener(_update);
+  }
+
+  _update() {
+    setState(() {});
   }
 
   ///单个视频窗口
@@ -33,5 +39,11 @@ class _VideoViewWidgetState extends State<VideoViewWidget> {
   @override
   Widget build(BuildContext context) {
     return _buildVideoView(context);
+  }
+
+  @override
+  void dispose() {
+    peerConnectionsController.removeListener(_update);
+    super.dispose();
   }
 }
