5506dcbc0a2d1ad0426f80c440421b46177302ef
hujs888 <hujs@curltech.io>
2023-03-23 13:29:34 +0800

    chat badge

--- lib/tool/document_util.dart
+++ lib/tool/document_util.dart
@@ -1,21 +1,21 @@
 import 'package:colla_chat/platform.dart';
 import 'package:colla_chat/widgets/common/platform_webview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_filereader/flutter_filereader.dart';
-
-class DocumentUtil {
-  /// IOS docx,doc,xlsx,xls,pptx,ppt,pdf,txt,jpg,jpeg,png
-  /// Android docx,doc,xlsx,xls,pptx,ppt,pdf,txt
-  static Widget buildFileReaderView({Key? key, required String filePath}) {
-    if (platformParams.mobile) {
-      FileReaderView view = FileReaderView(
-        key: key,
-        filePath: filePath,
-      );
-
-      return view;
-    } else {
-      return PlatformWebView(initialUrl: filePath);
-    }
-  }
-}
+// import 'package:flutter_filereader/flutter_filereader.dart';
+//
+// class DocumentUtil {
+//   /// IOS docx,doc,xlsx,xls,pptx,ppt,pdf,txt,jpg,jpeg,png
+//   /// Android docx,doc,xlsx,xls,pptx,ppt,pdf,txt
+//   static Widget buildFileReaderView({Key? key, required String filePath}) {
+//     if (platformParams.mobile) {
+//       FileReaderView view = FileReaderView(
+//         key: key,
+//         filePath: filePath,
+//       );
+//
+//       return view;
+//     } else {
+//       return PlatformWebView(initialUrl: filePath);
+//     }
+//   }
+// }
