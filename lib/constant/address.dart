012496cb9f6b7fce0a6c8b190675e677f5b0c274
hujs888 <hujs@curltech.io>
2023-03-25 22:41:26 +0800

    reinit-update org id to io.curltech

--- lib/constant/address.dart
+++ lib/constant/address.dart
@@ -6,49 +6,19 @@ final Map<String, PeerEndpoint> nodeAddressOptions = {
   'default': PeerEndpoint(
       name: 'default',
       priority: 0,
-      wsConnectAddress: 'wss://localhost:9090/websocket',
-      httpConnectAddress: 'https://localhost:9090',
-      peerId: '12D3KooWJvWPnHcYPEG3oPwDcobfc2wzWJsgKV7fj5LC7qx15ihS',
+      wsConnectAddress: 'wss://192.168.1.10:9090/websocket',
+      httpConnectAddress: 'https://192.168.1.10:9090',
+      peerId: '12D3KooWBiuFtWRQ5qrUmT5AFbJ6NXCqM9oKCMBUA3Dncm2mhLx8',
       libp2pConnectAddress:
-          '/ip4/127.0.0.1/tcp/5720/wss/p2p/12D3KooWJvWPnHcYPEG3oPwDcobfc2wzWJsgKV7fj5LC7qx15ihS',
+          '/ip4/192.168.1.10/tcp/5720/wss/p2p/12D3KooWBiuFtWRQ5qrUmT5AFbJ6NXCqM9oKCMBUA3Dncm2mhLx8',
       iceServers: [
         //{"url": "stun:stun.l.google.com:19302"},
         {
-          'url': 'stun:localhost:3478',
+          'url': 'stun:192.168.1.10:3478',
         },
         // {
-        //   'url': 'turn:localhost:3478',
+        //   'url': 'turn:192.168.1.10:3478',
         // }
-      ]),
-  'China-Shenzhen': PeerEndpoint(
-      name: 'China-Shenzhen',
-      wsConnectAddress: 'wss://sz.curltech.com.cn:9090/websocket',
-      httpConnectAddress: 'https://sz.curltech.com.cn:9090',
-      peerId: '12D3KooWFbyAYotJ3VtuDwCt1pkGdxGVxiW8PfiXsZ1NMqe1cKxJ',
-      libp2pConnectAddress:
-          '/dns4/sz.curltech.com.cn/tcp/5720/wss/p2p/12D3KooWFbyAYotJ3VtuDwCt1pkGdxGVxiW8PfiXsZ1NMqe1cKxJ',
-      iceServers: [
-        {
-          'urls': 'stun:sz.curltech.com.cn:3478',
-        },
-        {
-          'urls': 'turn:sz.curltech.com.cn:3478',
-        }
-      ]),
-  'China-Hangzhou': PeerEndpoint(
-      name: 'China-Hangzhou',
-      wsConnectAddress: 'wss://hz.curltech.com.cn:9090/websocket',
-      httpConnectAddress: 'https://hz.curltech.com.cn:9090',
-      peerId: '12D3KooWDTYWJ7bHXJFcEwq1NBJ2majgRjrY2xHjoMzSedkS3jgN',
-      libp2pConnectAddress:
-          '/dns4/hz.curltech.com.cn/tcp/5720/wss/p2p/12D3KooWDTYWJ7bHXJFcEwq1NBJ2majgRjrY2xHjoMzSedkS3jgN',
-      iceServers: [
-        {
-          'urls': 'stun:hz.curltech.com.cn:3478',
-        },
-        {
-          'urls': 'turn:hz.curltech.com.cn:3478',
-        }
       ]),
   'South Korea': PeerEndpoint(
       name: 'South Korea',
