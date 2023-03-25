f5e15ec8789237b11a8dc1e9f369dcf3a5147e6b
hujs888 <hujs@curltech.io>
2023-03-26 01:58:15 +0800

    ios permission

--- lib/datastore/condition_import/desktop.dart
+++ lib/datastore/condition_import/desktop.dart
@@ -9,12 +9,6 @@ import 'package:sqlite3/sqlite3.dart';
 
 Future<CommonDatabase> openSqlite3({String name = 'colla_chat.db'}) async {
   if (!platformParams.web) {
-    PermissionStatus status =
-        await PermissionUtil.requestPermission(Permission.storage);
-    if (status != PermissionStatus.granted) {
-      throw UnimplementedError('No storage permission');
-    }
-
     /// 除了web之外的创建打开数据库的方式
     final dbFolder = await PathUtil.getApplicationDirectory();
     String path = name;
