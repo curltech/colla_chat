8f672f9f20ce24dc42bd86f5a388bb83c420bd48
hujs888 <hujs@curltech.io>
2023-03-31 03:30:14 +0800

    remove nearby

--- lib/tool/share_util.dart
+++ lib/tool/share_util.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:colla_chat/plugin/logger.dart';
 import 'package:colla_chat/tool/xfile_util.dart';
 import 'package:cross_file/cross_file.dart';
-import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+// import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'package:share_plus/share_plus.dart';
 
 class ShareUtil {
@@ -44,36 +44,36 @@ class ShareUtil {
 
   /// https://github.com/KasemJaffer/receive_sharing_intent
   /// 设置app可以分享其他应用的文件和文本
-  static void sharedMediaStream(
-      Function(List<SharedMediaFile> files) receiver) {
-    // For sharing images coming from outside the app while the app is in the memory
-    ReceiveSharingIntent.getMediaStream().listen((List<SharedMediaFile> files) {
-      receiver(files);
-    }, onError: (err) {
-      logger.i("getIntentDataStream error: $err");
-    });
-  }
-
-  static void sharedMediaFile(Function(List<SharedMediaFile> files) receiver) {
-    // For sharing images coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> files) {
-      receiver(files);
-    });
-  }
-
-  static void sharedTextStream(Function(String value) receiver) {
-    // For sharing or opening urls/text coming from outside the app while the app is in the memory
-    ReceiveSharingIntent.getTextStream().listen((String value) {
-      receiver(value);
-    }, onError: (err) {
-      logger.i("getLinkStream error: $err");
-    });
-  }
-
-  static void sharedText(Function(String? value) receiver) {
-    // For sharing or opening urls/text coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialText().then((String? value) {
-      receiver(value);
-    });
-  }
+  // static void sharedMediaStream(
+  //     Function(List<SharedMediaFile> files) receiver) {
+  //   // For sharing images coming from outside the app while the app is in the memory
+  //   ReceiveSharingIntent.getMediaStream().listen((List<SharedMediaFile> files) {
+  //     receiver(files);
+  //   }, onError: (err) {
+  //     logger.i("getIntentDataStream error: $err");
+  //   });
+  // }
+  //
+  // static void sharedMediaFile(Function(List<SharedMediaFile> files) receiver) {
+  //   // For sharing images coming from outside the app while the app is closed
+  //   ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> files) {
+  //     receiver(files);
+  //   });
+  // }
+  //
+  // static void sharedTextStream(Function(String value) receiver) {
+  //   // For sharing or opening urls/text coming from outside the app while the app is in the memory
+  //   ReceiveSharingIntent.getTextStream().listen((String value) {
+  //     receiver(value);
+  //   }, onError: (err) {
+  //     logger.i("getLinkStream error: $err");
+  //   });
+  // }
+  //
+  // static void sharedText(Function(String? value) receiver) {
+  //   // For sharing or opening urls/text coming from outside the app while the app is closed
+  //   ReceiveSharingIntent.getInitialText().then((String? value) {
+  //     receiver(value);
+  //   });
+  // }
 }
