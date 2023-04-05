b744839f518cf0412f5bf112cb3facfe245af71e
hujs888 <hujs@curltech.io>
2023-04-05 19:20:46 +0800

    android package

---
+++ lib/tool/keyboard_util.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+
+/// Keyboard Visibility
+/// KeyboardVisibilityBuilder isKeyboardVisible
+/// KeyboardVisibilityProvider final bool isKeyboardVisible = KeyboardVisibilityProvider.isKeyboardVisible(context);
+class KeyboardVisibilityUtil {
+  static bool isVisible() {
+    var keyboardVisibilityController = KeyboardVisibilityController();
+
+    return keyboardVisibilityController.isVisible;
+  }
+
+  static StreamSubscription<bool> listen(Function(bool visible) onChange) {
+    var keyboardVisibilityController = KeyboardVisibilityController();
+    StreamSubscription<bool> keyboardSubscription =
+        keyboardVisibilityController.onChange.listen((bool visible) {
+      onChange(visible);
+    });
+
+    return keyboardSubscription;
+  }
+}
