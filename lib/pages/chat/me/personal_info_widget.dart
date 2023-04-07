e892992d11a911885627fa9e326c320823ce89d8
hujs888 <hujs@curltech.io>
2023-04-08 04:06:01 +0800

    auto size text

--- lib/pages/chat/me/personal_info_widget.dart
+++ lib/pages/chat/me/personal_info_widget.dart
@@ -66,7 +66,7 @@ class _PersonalInfoWidgetState extends State<PersonalInfoWidget>
               .navigateTo(context, Application.p2pLogin, replace: true);
         },
         child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          CommonAutoSizeText(AppLocalizations.t('Logout')),
+          Expanded(child: CommonAutoSizeText(AppLocalizations.t('Logout'))),
           const SizedBox(
             width: 5,
           ),
