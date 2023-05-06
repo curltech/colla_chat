d3426177b5f722499177ab4c88cb688a1b216489
hujs888 <hujs@curltech.io>
2023-05-07 01:32:17 +0800

    message

--- lib/pages/chat/chat/message/file_message.dart
+++ lib/pages/chat/chat/message/file_message.dart
@@ -1,5 +1,6 @@
 import 'package:colla_chat/pages/chat/chat/message/common_message.dart';
 import 'package:colla_chat/provider/myself.dart';
+import 'package:colla_chat/tool/file_util.dart';
 import 'package:colla_chat/widgets/data_bind/data_listtile.dart';
 import 'package:flutter/material.dart';
 import 'package:mimecon/mimecon.dart';
@@ -21,9 +22,10 @@ class FileMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String? mimeType = FileUtil.mimeType(title);
     var tileData = TileData(
       prefix: Mimecon(
-        mimetype: mimeType,
+        mimetype: mimeType ?? this.mimeType,
         color: myself.primary,
         size: 36,
         isOutlined: true,
