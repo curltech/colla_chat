0b8b84ef5fe865477803b86237693013437df4a0
hujs888 <hujs@curltech.io>
2023-05-16 01:25:28 +0800

    message

--- lib/service/servicelocator.dart
+++ lib/service/servicelocator.dart
@@ -35,7 +35,6 @@ import 'package:colla_chat/service/general_base.dart';
 import 'package:colla_chat/service/p2p/security_context.dart';
 import 'package:colla_chat/service/stock/account.dart';
 import 'package:colla_chat/tool/json_util.dart';
-import 'package:colla_chat/transport/smsclient.dart';
 
 class ServiceLocator {
   static Map<String, GeneralBaseService> services = {};
@@ -106,7 +105,6 @@ class ServiceLocator {
     bool loginStatus = await myselfPeerService.autoLogin();
     logger.i(
         'AutoLogin setting:${appDataProvider.autoLogin},AutoLogin status:$loginStatus');
-    var s = smsClient;
 
     return loginStatus;
   }
