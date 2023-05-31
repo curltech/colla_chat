ff69d2fbeadb2e621b3b3a612daa2089d733fb78
hujs888 <hujs@curltech.io>
2023-06-01 01:20:01 +0800

    message

--- lib/tool/video_util.dart
+++ lib/tool/video_util.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:colla_chat/tool/file_util.dart';
 import 'package:fc_native_video_thumbnail/fc_native_video_thumbnail.dart';
+import 'package:video_compress/video_compress.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 
 class VideoUtil {
@@ -55,6 +57,7 @@ class VideoUtil {
     return file;
   }
 
+  ///支持ANDROID，IOS，MACOS，WINDOWS
   static Future<Uint8List?> videoThumbnailData({
     required String videoFile,
     int width = 1024,
@@ -96,5 +99,78 @@ class VideoUtil {
         keepAspectRatio: keepAspectRatio,
         format: format,
         quality: quality);
+  }
+
+  ///ANDROID,IOS,MACOS
+  static Future<MediaInfo?> compressVideo(
+    String path, {
+    VideoQuality quality = VideoQuality.DefaultQuality,
+    bool deleteOrigin = false,
+    int? startTime,
+    int? duration,
+    bool? includeAudio,
+    int frameRate = 30,
+  }) async {
+    MediaInfo? mediaInfo = await VideoCompress.compressVideo(
+      path,
+      quality: quality,
+      deleteOrigin: deleteOrigin,
+      startTime: startTime,
+      duration: duration,
+      includeAudio: includeAudio,
+      frameRate: frameRate,
+    );
+
+    return mediaInfo;
+  }
+
+  ///ANDROID,IOS,MACOS
+  static Future<Uint8List?> getByteThumbnail(
+    String path, {
+    int quality = 10,
+    int position = -1,
+  }) async {
+    final thumbnail = await VideoCompress.getByteThumbnail(path,
+        quality: quality, // default(100)
+        position: position // default(-1)
+        );
+
+    return thumbnail;
+  }
+
+  ///ANDROID,IOS,MACOS
+  static Future<File> getFileThumbnail(
+    String path, {
+    int quality = 10,
+    int position = -1,
+  }) async {
+    final thumbnailFile = await VideoCompress.getFileThumbnail(path,
+        quality: quality, // default(100)
+        position: position // default(-1)
+        );
+
+    return thumbnailFile;
+  }
+
+  ///ANDROID,IOS,MACOS
+  static Future<MediaInfo> getMediaInfo(String path) async {
+    final info = await VideoCompress.getMediaInfo(path);
+
+    return info;
+  }
+
+  ///ANDROID,IOS,MACOS
+  static Future<bool?> deleteAllCache() async {
+    return await VideoCompress.deleteAllCache();
+  }
+
+  ///ANDROID,IOS,MACOS
+  static Subscription compressProgress(Function(double progress) fn) {
+    Subscription subscription =
+        VideoCompress.compressProgress$.subscribe((double progress) {
+      fn(progress);
+    });
+
+    return subscription;
   }
 }
