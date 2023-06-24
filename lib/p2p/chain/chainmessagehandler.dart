5337ff4163cb0c089b6abf3564bda982ecdf5aa4
hujs888 <hujs@colla.cc>
2023-06-24 16:54:14 +0800

    macos

--- lib/p2p/chain/chainmessagehandler.dart
+++ lib/p2p/chain/chainmessagehandler.dart
@@ -159,7 +159,10 @@ class ChainMessageHandler {
           targetConnectAddress.startsWith('ws')) {
         var websocket = await websocketPool.get(targetConnectAddress);
         // var websocket = websocketPool.getDefault();
-        if (websocket != null) {
+        if (websocket == null || websocket.status != SocketStatus.connected) {
+          Future.delayed(const Duration(seconds: 1));
+        }
+        if (websocket != null && websocket.status == SocketStatus.connected) {
           var data = MessageSerializer.marshal(chainMessage);
           success = await websocket.sendMsg(data);
         }
@@ -168,7 +171,10 @@ class ChainMessageHandler {
           connectAddress != null &&
           connectAddress.startsWith('ws')) {
         var websocket = await websocketPool.get(connectAddress);
-        if (websocket != null) {
+        if (websocket == null || websocket.status != SocketStatus.connected) {
+          Future.delayed(const Duration(seconds: 1));
+        }
+        if (websocket != null && websocket.status == SocketStatus.connected) {
           var data = MessageSerializer.marshal(chainMessage);
           success = await websocket.sendMsg(data);
         }
