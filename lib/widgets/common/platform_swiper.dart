b945a25beb27942272c8f36f54603882866131e9
hujs888 <hujs@colla.cc>
2023-06-25 05:04:17 +0800

    macos

--- lib/widgets/common/platform_swiper.dart
+++ lib/widgets/common/platform_swiper.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
+import 'package:another_transformer_page_view/another_transformer_page_view.dart';
 import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:colla_chat/tool/string_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'dart:math' as Math;
+
+import 'package:vector_math/vector_math_64.dart';
 
 enum PlatformSwiperDirection { none, left, right, top, bottom }
 
@@ -125,6 +129,25 @@ class _PlatformSwiperState extends State<PlatformSwiper> {
         ));
   }
 
+  Widget _buildTransformerPageView() {
+    return TransformerPageView(
+        key: widget.key,
+        index: widget.index,
+        duration: Duration(milliseconds: widget.duration),
+        loop: widget.loop,
+        curve: widget.curve,
+        viewportFraction: widget.viewportFraction,
+        scrollDirection: widget.scrollDirection,
+        physics: widget.physics,
+        // pageSnapping: widget.pageSnapping,
+        // onPageChanged: widget.onPageChanged,
+        // controller: widget.controller,
+        pageController: TransformerPageController(),
+        transformer: ThreeDTransformer(),
+        itemBuilder: widget.itemBuilder,
+        itemCount: widget.itemCount);
+  }
+
   FutureOr<bool> onCardSwipe(
       int currentIndex, int? oldIndex, CardSwiperDirection direction) {
     if (widget.onSwipe != null) {
@@ -205,5 +228,179 @@ class _PlatformSwiperState extends State<PlatformSwiper> {
   @override
   Widget build(BuildContext context) {
     return _buildSwiper();
+  }
+}
+
+class AccordionTransformer extends PageTransformer {
+  @override
+  Widget transform(Widget child, TransformInfo info) {
+    double? position = info.position;
+    if (position! < 0.0) {
+      return Transform.scale(
+        scale: 1 + position,
+        alignment: Alignment.topRight,
+        child: child,
+      );
+    } else {
+      return Transform.scale(
+        scale: 1 - position,
+        alignment: Alignment.bottomLeft,
+        child: child,
+      );
+    }
+  }
+}
+
+class ThreeDTransformer extends PageTransformer {
+  @override
+  Widget transform(Widget child, TransformInfo info) {
+    double? position = info.position;
+    double? height = info.height;
+    double? width = info.width;
+    double pivotX = 0.0;
+    if (position! < 0 && position! >= -1) {
+      // left scrolling
+      pivotX = width!;
+    }
+    return Transform(
+      transform: Matrix4.identity()
+        ..rotate(Vector3(0.0, 2.0, 0.0), position * 1.5),
+      origin: Offset(pivotX, height! / 2),
+      child: child,
+    );
+  }
+}
+
+class ZoomInPageTransformer extends PageTransformer {
+  static const double ZOOM_MAX = 0.5;
+
+  @override
+  Widget transform(Widget child, TransformInfo info) {
+    double? position = info.position;
+    double? width = info.width;
+    if (position! > 0 && position! <= 1) {
+      return Transform.translate(
+        offset: Offset(-width! * position, 0.0),
+        child: Transform.scale(
+          scale: 1 - position,
+          child: child,
+        ),
+      );
+    }
+    return child;
+  }
+}
+
+class ZoomOutPageTransformer extends PageTransformer {
+  static const double MIN_SCALE = 0.85;
+  static const double MIN_ALPHA = 0.5;
+
+  @override
+  Widget transform(Widget child, TransformInfo info) {
+    double? position = info.position;
+    double? pageWidth = info.width;
+    double? pageHeight = info.height;
+
+    if (position! < -1) {
+      // [-Infinity,-1)
+      // This page is way off-screen to the left.
+      //view.setAlpha(0);
+    } else if (position <= 1) {
+      // [-1,1]
+      // Modify the default slide transition to
+      // shrink the page as well
+      double scaleFactor = Math.max(MIN_SCALE, 1 - position.abs());
+      double vertMargin = pageHeight! * (1 - scaleFactor) / 2;
+      double horzMargin = pageWidth! * (1 - scaleFactor) / 2;
+      double dx;
+      if (position < 0) {
+        dx = (horzMargin - vertMargin / 2);
+      } else {
+        dx = (-horzMargin + vertMargin / 2);
+      }
+      // Scale the page down (between MIN_SCALE and 1)
+      double opacity = MIN_ALPHA +
+          (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA);
+
+      return Opacity(
+        opacity: opacity,
+        child: Transform.translate(
+          offset: Offset(dx, 0.0),
+          child: Transform.scale(
+            scale: scaleFactor,
+            child: child,
+          ),
+        ),
+      );
+    } else {
+      // (1,+Infinity]
+      // This page is way off-screen to the right.
+      // view.setAlpha(0);
+    }
+
+    return child;
+  }
+}
+
+class DeepthPageTransformer extends PageTransformer {
+  DeepthPageTransformer() : super(reverse: true);
+
+  @override
+  Widget transform(Widget child, TransformInfo info) {
+    double? position = info.position;
+    if (position! <= 0) {
+      return Opacity(
+        opacity: 1.0,
+        child: Transform.translate(
+          offset: const Offset(0.0, 0.0),
+          child: Transform.scale(
+            scale: 1.0,
+            child: child,
+          ),
+        ),
+      );
+    } else if (position <= 1) {
+      const double MIN_SCALE = 0.75;
+      // Scale the page down (between MIN_SCALE and 1)
+      double scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - position);
+
+      return Opacity(
+        opacity: 1.0 - position,
+        child: Transform.translate(
+          offset: Offset(info.width! * -position, 0.0),
+          child: Transform.scale(
+            scale: scaleFactor,
+            child: child,
+          ),
+        ),
+      );
+    }
+
+    return child;
+  }
+}
+
+class ScaleAndFadeTransformer extends PageTransformer {
+  final double _scale;
+  final double _fade;
+
+  ScaleAndFadeTransformer({double fade: 0.3, double scale: 0.8})
+      : _fade = fade,
+        _scale = scale;
+
+  @override
+  Widget transform(Widget item, TransformInfo info) {
+    double? position = info.position;
+    double scaleFactor = (1 - position!.abs()) * (1 - _scale);
+    double fadeFactor = (1 - position!.abs()) * (1 - _fade);
+    double opacity = _fade + fadeFactor;
+    double scale = _scale + scaleFactor;
+    return Opacity(
+      opacity: opacity,
+      child: Transform.scale(
+        scale: scale,
+        child: item,
+      ),
+    );
   }
 }
