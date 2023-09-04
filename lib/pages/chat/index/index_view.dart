2d3313e18357138321e8df2cdacdd4fe23b8e260
hujs888 <hujs@colla.cc>
2023-09-04 15:38:19 +0800

    update find linkman

--- lib/pages/chat/index/index_view.dart
+++ lib/pages/chat/index/index_view.dart
@@ -384,7 +384,7 @@ class _IndexViewState extends State<IndexView>
         height: 148,
         alignment: Alignment.topLeft,
         width: appDataProvider.totalSize.width,
-        padding: const EdgeInsets.all(0.0),
+        padding: const EdgeInsets.all(10.0),
         child: Card(
             elevation: 0.0,
             margin: EdgeInsets.zero,
