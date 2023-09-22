759d5d5837ef78a6ff01fc163bd7b056163bff58
hujs888 <hujs@colla.cc>
2023-09-22 21:27:43 +0800

    add notification

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by jingsong Hu on 2023-09-22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
