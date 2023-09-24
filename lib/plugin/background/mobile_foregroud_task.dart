fad6f9969a9a76aecd31cf00f279f98d7f9ccacc
hujs888 <hujs@colla.cc>
2023-09-24 23:56:34 +0800

    video chat

--- lib/plugin/background/mobile_foregroud_task.dart
+++ lib/plugin/background/mobile_foregroud_task.dart
@@ -12,6 +12,7 @@ import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
 ///支持android和ios，在单独线程中执行一些前台任务
 ///只要前台任务运行，则应用不会被关闭
+/// iOS有一些限制：app被强制终止时，设备重启时，任务不会工作，onRepeatEvent可能不能正常工作
 class MobileForegroundTask {
   ///接收数据端口，接收前台服务任务发送来的数据
   ReceivePort? receivePort;
@@ -48,6 +49,7 @@ class MobileForegroundTask {
       AppLocalizations.t('CollaChat foreground Service is running');
   String notificationText = AppLocalizations.t('Tap to return to the app');
 
+  /// 初始化前台任务
   init() {
     FlutterForegroundTask.init(
       androidNotificationOptions: androidNotificationOptions,
@@ -81,6 +83,7 @@ class MobileForegroundTask {
     }
   }
 
+  /// 手工启动任务
   Future<bool> start() async {
     /// 存储数据到前台任务线程
     await FlutterForegroundTask.saveData(key: 'customData', value: 'hello');
@@ -104,6 +107,7 @@ class MobileForegroundTask {
     }
   }
 
+  /// 注册接收端口
   bool registerReceivePort(ReceivePort? newReceivePort) {
     newReceivePort ??= FlutterForegroundTask.receivePort;
     if (newReceivePort == receivePort) {
@@ -121,6 +125,7 @@ class MobileForegroundTask {
     return receivePort != null;
   }
 
+  /// 更新服务任务参数
   Future<bool> updateService() {
     return FlutterForegroundTask.updateService(
       foregroundTaskOptions: foregroundTaskOptions,
@@ -130,12 +135,13 @@ class MobileForegroundTask {
     );
   }
 
+  /// 停止任务
   Future<bool> stop() {
     return FlutterForegroundTask.stopService();
   }
 
   ///创建WillStartForegroundTask组件，MaterialApp的home，用于包裹Scaffold
-  ///当应用最小化或者终止的时候，这个组件能够启动前台服务
+  ///当应用最小化或者终止的时候，这个组件能够启动前台服务，这是自动启动方式的时候使用
   WillStartForegroundTask willStartForegroundTask({
     Key? key,
     Future<bool> Function()? onWillStart,
@@ -177,7 +183,7 @@ class MobileForegroundTask {
   }
 
   ///创建WillForegroundTask组件，用于包裹Scaffold，使用前需要先调用初始化init方法
-  ///当前台服务运行时，防止应用被关闭
+  ///当前台服务运行时，防止应用被关闭，这是手工启动任务的时候使用
   WithForegroundTask withForegroundTask({Key? key, required Widget child}) {
     return WithForegroundTask(
       key: key,
@@ -185,71 +191,91 @@ class MobileForegroundTask {
     );
   }
 
+  ///最小化app
   minimizeApp() {
     FlutterForegroundTask.minimizeApp();
   }
 
+  ///如果app没有关闭，启动app
   launchApp([String? route]) {
     FlutterForegroundTask.launchApp(route);
   }
 
+  ///屏幕关闭的时候打开屏幕
   wakeUpScreen() {
     FlutterForegroundTask.wakeUpScreen();
   }
 
+  /// 应用是否在前台
   Future<bool> get isAppOnForeground async {
     return await FlutterForegroundTask.isAppOnForeground;
   }
 
+  ///切换锁定屏幕的可见性
   setOnLockScreenVisibility(bool isVisible) {
     FlutterForegroundTask.setOnLockScreenVisibility(isVisible);
+  }
+
+  /// 从SharedPreferences获取数据
+  Future<T?> getData<T>(String key) {
+    return FlutterForegroundTask.getData(key: key);
+  }
+
+  /// 保存数据到SharedPreferences
+  saveData(String key, Object value) async {
+    await FlutterForegroundTask.saveData(key: key, value: value);
   }
 }
 
 final MobileForegroundTask mobileForegroundTask = MobileForegroundTask();
 
-///实际的任务处理器
+/// 实际的前台任务处理器
 class MobileForegroundTaskHandler extends TaskHandler {
+  //启动后的发送端口
   SendPort? _sendPort;
 
   /// 任务开始的时候调用
   @override
   void onStart(DateTime timestamp, SendPort? sendPort) async {
     _sendPort = sendPort;
-
-    // You can use the getData function to get the stored data.
-    final customData =
-        await FlutterForegroundTask.getData<String>(key: 'customData');
+    logger.w('MobileForegroundTask started');
   }
 
   // 周期任务调用 [ForegroundTaskOptions].
   @override
   void onRepeatEvent(DateTime timestamp, SendPort? sendPort) async {
+    logger.w('MobileForegroundTask onRepeatEvent');
     // 发送数据给主线程
     sendPort?.send(timestamp);
+    logger.w('MobileForegroundTask send data');
   }
 
   /// 通知按钮被按的时候调用，android平台
   @override
-  void onDestroy(DateTime timestamp, SendPort? sendPort) async {}
+  void onDestroy(DateTime timestamp, SendPort? sendPort) async {
+    logger.w('MobileForegroundTask onDestroy');
+  }
 
   /// 通知按钮被按的时候调用，android平台
   @override
-  void onNotificationButtonPressed(String id) {}
+  void onNotificationButtonPressed(String id) {
+    logger.w('MobileForegroundTask onNotificationButtonPressed');
+  }
 
   /// 通知被按的时候调用，android平台
-  /// "android.permission.SYSTEM_ALERT_WINDOW" permission must be granted for
-  /// this function to be called.
+  /// 必须有"android.permission.SYSTEM_ALERT_WINDOW"权限
   @override
   void onNotificationPressed() {
+    logger.w('MobileForegroundTask onNotificationPressed');
     // 应用退出的时候重启应用，并且发送数据到应用
     FlutterForegroundTask.launchApp();
-    _sendPort?.send('onNotificationPressed');
+    logger.w('MobileForegroundTask launchApp');
   }
 }
 
 ///服务线程启动，在单独的服务线程中执行的代码
 @pragma('vm:entry-point')
 void onStart() async {
+  logger.w('MobileForegroundTask entry-point onStart');
   FlutterForegroundTask.setTaskHandler(MobileForegroundTaskHandler());
 }
