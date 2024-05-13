0885648d1532937a53eb0af3bd5283e2d3d07b0a
hujs888 <hujs@colla.cc>
2024-05-14 04:20:49 +0800

    little bug

--- lib/pages/chat/me/poem/poem_widget.dart
+++ lib/pages/chat/me/poem/poem_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:colla_chat/l10n/localization.dart';
 import 'package:colla_chat/plugin/talker_logger.dart';
 import 'package:colla_chat/provider/myself.dart';
@@ -36,27 +35,12 @@ class _PoemWidgetState extends State<PoemWidget>
     super.initState();
   }
 
-  Widget _buildParser(BuildContext context) {
-    ButtonStyle style = StyleUtil.buildButtonStyle(
-        maximumSize: const Size(140.0, 56.0), backgroundColor: myself.primary);
-    return TextButton.icon(
-      style: style,
-      icon: const Icon(Icons.exit_to_app),
-      label: CommonAutoSizeText(AppLocalizations.t('parse')),
-      onPressed: () async {
-        await poemService
-            .parseJson('/Users/jingsonghu/Downloads/chinese-poetry-master');
-        logger.i('parse json completely!');
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     var personalInfo = AppBarView(
       title: widget.title,
       withLeading: widget.withLeading,
-      child: _buildParser(context),
+      child: Container(),
     );
 
     return personalInfo;
