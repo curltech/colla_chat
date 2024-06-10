62a3da1c5fdcf5dcba9c795cd24d2f0df936f72d
hujs888 <hujs@colla.cc>
2024-06-10 08:13:20 +0800

    little bug

--- lib/pages/chat/login/p2p_login.dart
+++ lib/pages/chat/login/p2p_login.dart
@@ -1,4 +1,5 @@
 import 'package:card_swiper/card_swiper.dart';
+import 'package:colla_chat/entity/dht/myselfpeer.dart';
 import 'package:colla_chat/l10n/localization.dart';
 import 'package:colla_chat/pages/chat/login/loading.dart';
 import 'package:colla_chat/pages/chat/login/p2p_login_widget.dart';
@@ -11,6 +12,8 @@ import 'package:colla_chat/widgets/common/common_widget.dart';
 import 'package:colla_chat/widgets/style/platform_widget_factory.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+
+import '../me/settings/advanced/myselfpeer/myself_peer_controller.dart';
 
 /// 远程登录页面，一个Scaffold，IndexStack下的远程登录组件，注册组件和配置组件
 class P2pLogin extends StatefulWidget {
@@ -49,6 +52,10 @@ class _P2pLoginState extends State<P2pLogin> {
     super.initState();
     myself.addListener(_update);
     appDataProvider.addListener(_update);
+    List<MyselfPeer> myselfPeers = myselfPeerController.data;
+    if (myselfPeers.isEmpty) {
+      index = 1;
+    }
   }
 
   _update() {
