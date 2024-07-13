dd9f1a8540f624e8cbd96fa7905b5588de73597d
hujs888 <hujs@colla.cc>
2024-07-14 06:30:45 +0800

    little bug

--- lib/pages/poem/poem_widget.dart
+++ lib/pages/poem/poem_widget.dart
@@ -4,12 +4,14 @@ import 'package:card_swiper/card_swiper.dart';
 import 'package:colla_chat/entity/poem/poem.dart';
 import 'package:colla_chat/l10n/localization.dart';
 import 'package:colla_chat/plugin/talker_logger.dart';
+import 'package:colla_chat/plugin/text_to_speech_widget.dart';
 import 'package:colla_chat/provider/app_data_provider.dart';
 import 'package:colla_chat/provider/data_list_controller.dart';
 import 'package:colla_chat/provider/myself.dart';
 import 'package:colla_chat/service/poem/poem.dart';
 import 'package:colla_chat/tool/dialog_util.dart';
 import 'package:colla_chat/tool/file_util.dart';
+import 'package:colla_chat/tool/loading_util.dart';
 import 'package:colla_chat/tool/string_util.dart';
 import 'package:colla_chat/widgets/common/app_bar_view.dart';
 import 'package:colla_chat/widgets/common/common_text_form_field.dart';
@@ -19,6 +21,7 @@ import 'package:colla_chat/widgets/data_bind/data_field_widget.dart';
 import 'package:colla_chat/widgets/data_bind/data_listtile.dart';
 import 'package:colla_chat/widgets/data_bind/data_listview.dart';
 import 'package:colla_chat/widgets/data_bind/form_input_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
 
@@ -79,6 +82,8 @@ class PoemWidget extends StatelessWidget with TileDataMixin {
 
   DataListController<Poem> poemController = DataListController<Poem>();
 
+  ValueNotifier<Poem?> poem = ValueNotifier<Poem?>(null);
+
   ExpansionTileController expansionTileController = ExpansionTileController();
 
   bool isExpanded() {
@@ -135,13 +140,20 @@ class PoemWidget extends StatelessWidget with TileDataMixin {
     } catch (e) {
       logger.e('collapse failure:$e');
     }
-    List<Poem> poems = await poemService.sendSearchPoem(
-        title: title,
-        author: author,
-        rhythmic: rhythmic,
-        paragraphs: paragraphs,
-        from: 0,
-        limit: 100);
+    List<Poem> poems = [];
+    DialogUtil.loadingShow(context);
+    try {
+      poems = await poemService.sendSearchPoem(
+          title: title,
+          author: author,
+          rhythmic: rhythmic,
+          paragraphs: paragraphs,
+          from: 0,
+          limit: 100);
+    } catch (e) {
+      logger.e('sendSearchPoem failure:$e');
+    }
+    DialogUtil.loadingHide(context);
     poemController.replaceAll(poems);
   }
 
@@ -178,6 +190,8 @@ class PoemWidget extends StatelessWidget with TileDataMixin {
 
   int index = 0;
 
+  TextToSpeechWidget textToSpeechWidget = TextToSpeechWidget();
+
   Widget _buildPoemListWidget(BuildContext context) {
     return Column(children: [
       const SizedBox(
@@ -200,91 +214,129 @@ class PoemWidget extends StatelessWidget with TileDataMixin {
                 titleTail: '${poem.dynasty} ${poem.author}',
                 onTap: (int index, String title, {String? subtitle}) {
                   poemController.currentIndex = index;
+                  this.poem.value = poemController.current;
                   swiperController.move(1);
                 },
               ));
               i++;
             }
+            return DataListView(
+              itemCount: tiles.length,
+              itemBuilder: (BuildContext context, int index) {
+                return tiles[index];
+              },
+            );
           }
-          return DataListView(
-            itemCount: tiles.length,
-            itemBuilder: (BuildContext context, int index) {
-              return tiles[index];
-            },
-          );
+          return Center(
+              child: CommonAutoSizeText(AppLocalizations.t('No poem')));
         },
       )),
     ]);
   }
 
   Widget _buildPoemContent(BuildContext context) {
-    Poem? poem = poemController.current;
-    if (poem != null && poem.paragraphs != null) {
-      List<String> titles = poem.title.split('。');
-      List<Widget> titleWidgets = [];
-      int i = 0;
-      for (var title in titles) {
-        if (i == 0) {
-          titleWidgets.add(CommonAutoSizeText(
-            title,
-            style: const TextStyle(fontSize: 28),
-          ));
-        } else {
-          titleWidgets.add(CommonAutoSizeText(
-            title,
-            style: const TextStyle(fontSize: 12),
-          ));
-        }
-        i++;
-      }
-      final reg = RegExp(r'[。|？|；|！|：]');
-      List<String> paragraphs = poem.paragraphs!.split(reg);
-      List<Widget> paragraphWidgets = [];
-      for (var paragraph in paragraphs) {
-        paragraphWidgets.add(CommonAutoSizeText(
-          paragraph,
-          style: const TextStyle(fontSize: 16),
-        ));
-      }
-      return Column(
-        children: [
-          ButtonBar(
-            alignment: MainAxisAlignment.start,
+    return ValueListenableBuilder(
+      valueListenable: poem,
+      builder: (BuildContext context, poem, Widget? child) {
+        if (poem != null && poem.paragraphs != null) {
+          List<String> titles = poem.title.split('。');
+          List<Widget> titleWidgets = [];
+          int i = 0;
+          for (var title in titles) {
+            if (i == 0) {
+              titleWidgets.add(CommonAutoSizeText(
+                title,
+                style: const TextStyle(fontSize: 28),
+              ));
+            } else {
+              titleWidgets.add(CommonAutoSizeText(
+                title,
+                style: const TextStyle(fontSize: 12),
+              ));
+            }
+            i++;
+          }
+          final reg = RegExp(r'[。|？|；|！|：]');
+          List<String> paragraphs = poem.paragraphs!.split(reg);
+          List<Widget> paragraphWidgets = [];
+          for (var paragraph in paragraphs) {
+            paragraphWidgets.add(CommonAutoSizeText(
+              paragraph,
+              style: const TextStyle(fontSize: 16),
+            ));
+          }
+          return Column(
             children: [
-              IconButton(
-                  hoverColor: myself.primary,
-                  onPressed: () {
-                    swiperController.move(0);
-                  },
-                  icon: const Icon(Icons.keyboard_arrow_left))
-            ],
-          ),
-          Expanded(
-              child: Column(
-            children: [
-              ...titleWidgets,
-              const SizedBox(
-                height: 15,
-              ),
-              CommonAutoSizeText('${poem.dynasty} ${poem.author}'),
-              const SizedBox(
-                height: 15,
+              ButtonBar(
+                alignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      color: Colors.white,
+                      hoverColor: myself.primary,
+                      onPressed: () {
+                        swiperController.move(0);
+                      },
+                      icon: const Icon(Icons.keyboard_arrow_left)),
+                  ValueListenableBuilder(
+                    valueListenable: textToSpeechWidget.ttsState,
+                    builder: (BuildContext context, ttsState, Widget? child) {
+                      return IconButton(
+                          color: Colors.white,
+                          hoverColor: myself.primary,
+                          onPressed: () {
+                            if (ttsState == TtsState.stopped) {
+                              textToSpeechWidget.speak(poem.paragraphs!);
+                            }
+                            if (ttsState == TtsState.paused) {
+                              textToSpeechWidget.speak(poem.paragraphs!);
+                            }
+                            if (ttsState == TtsState.playing) {
+                              textToSpeechWidget.pause();
+                            }
+                          },
+                          icon: ttsState == TtsState.stopped ||
+                                  ttsState == TtsState.paused
+                              ? const Icon(Icons.play_arrow)
+                              : const Icon(Icons.pause));
+                    },
+                  ),
+                  IconButton(
+                      color: Colors.white,
+                      hoverColor: myself.primary,
+                      onPressed: () {
+                        textToSpeechWidget.stop();
+                      },
+                      icon: const Icon(Icons.stop)),
+                ],
               ),
               Expanded(
-                  child: SingleChildScrollView(
-                      child: SizedBox(
-                          width: appDataProvider.secondaryBodyWidth,
-                          child: Column(
-                            children: [
-                              ...paragraphWidgets,
-                            ],
-                          )))),
+                  child: Column(
+                children: [
+                  ...titleWidgets,
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  CommonAutoSizeText('${poem.dynasty} ${poem.author}'),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Expanded(
+                      child: SingleChildScrollView(
+                          child: SizedBox(
+                              width: appDataProvider.secondaryBodyWidth,
+                              child: Column(
+                                children: [
+                                  ...paragraphWidgets,
+                                ],
+                              )))),
+                ],
+              )),
             ],
-          )),
-        ],
-      );
-    }
-    return Container();
+          );
+        }
+        return Container();
+      },
+    );
   }
 
   @override
---
+++ lib/plugin/text_to_speech_widget.dart
@@ -0,0 +1,347 @@
+import 'dart:async';
+
+import 'package:colla_chat/l10n/localization.dart';
+import 'package:colla_chat/platform.dart';
+import 'package:colla_chat/widgets/common/common_widget.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+enum TtsState { playing, stopped, paused, continued }
+
+class TextToSpeechWidget extends StatelessWidget {
+  late FlutterTts flutterTts;
+  ValueNotifier<String?> language = ValueNotifier<String?>(null);
+  ValueNotifier<String?> engine = ValueNotifier<String?>(null);
+  ValueNotifier<double> volume = ValueNotifier<double>(0.5);
+  ValueNotifier<double> pitch = ValueNotifier<double>(1.0);
+  ValueNotifier<double> rate = ValueNotifier<double>(0.2);
+  bool isCurrentLanguageInstalled = false;
+
+  ValueNotifier<TtsState> ttsState = ValueNotifier<TtsState>(TtsState.stopped);
+
+  TextToSpeechWidget({super.key}) {
+    initTts();
+  }
+
+  dynamic initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions(true);
+
+    if (platformParams.android) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    if (platformParams.ios) {
+      flutterTts.setSharedInstance(true);
+      flutterTts.setIosAudioCategory(
+          IosTextToSpeechAudioCategory.ambient,
+          [
+            IosTextToSpeechAudioCategoryOptions.allowBluetooth,
+            IosTextToSpeechAudioCategoryOptions.allowBluetoothA2DP,
+            IosTextToSpeechAudioCategoryOptions.mixWithOthers
+          ],
+          IosTextToSpeechAudioMode.voicePrompt);
+    }
+
+    flutterTts.setStartHandler(() {
+      ttsState.value = TtsState.playing;
+    });
+
+    flutterTts.setCompletionHandler(() {
+      ttsState.value = TtsState.stopped;
+    });
+
+    flutterTts.setCancelHandler(() {
+      ttsState.value = TtsState.stopped;
+    });
+
+    flutterTts.setPauseHandler(() {
+      ttsState.value = TtsState.paused;
+    });
+
+    flutterTts.setContinueHandler(() {
+      ttsState.value = TtsState.continued;
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      ttsState.value = TtsState.stopped;
+    });
+  }
+
+  /// 获取所有的语言
+  Future<dynamic> _getLanguages() async => await flutterTts.getLanguages;
+
+  /// 获取所有的引擎
+  Future<dynamic> _getEngines() async => await flutterTts.getEngines;
+
+  /// 获取缺省引擎
+  Future<dynamic> _getDefaultEngine() async {
+    return await flutterTts.getDefaultEngine;
+  }
+
+  /// 获取缺省的语音
+  Future<dynamic> _getDefaultVoice() async {
+    return await flutterTts.getDefaultVoice;
+  }
+
+  Future<int?> getMaxSpeechInputLength() async {
+    return await flutterTts.getMaxSpeechInputLength;
+  }
+
+  /// 设置等待选项
+  Future<void> _setAwaitOptions(bool wait) async {
+    await flutterTts.awaitSpeakCompletion(wait);
+  }
+
+  Future<void> _setAwaitSynthCompletion(bool wait) async {
+    await flutterTts.awaitSynthCompletion(wait);
+  }
+
+  Future<void> _synthesizeToFile(String text, String filename) async {
+    await flutterTts.synthesizeToFile(
+        text, platformParams.android ? "$filename.wav" : "$filename.caf");
+  }
+
+  /// 播放
+  Future<void> speak(String text) async {
+    await flutterTts.setVolume(volume.value);
+    await flutterTts.setSpeechRate(rate.value);
+    await flutterTts.setPitch(pitch.value);
+
+    await flutterTts.speak(text);
+  }
+
+  /// 停止
+  Future<void> stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) ttsState.value = TtsState.stopped;
+  }
+
+  ///暂停
+  Future<void> pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) ttsState.value = TtsState.paused;
+  }
+
+  List<DropdownMenuItem<String>> getEnginesDropDownMenuItems(
+      List<dynamic> engines) {
+    var items = <DropdownMenuItem<String>>[];
+    for (dynamic type in engines) {
+      items.add(DropdownMenuItem(
+          value: type as String?, child: Text((type as String))));
+    }
+    return items;
+  }
+
+  void changeEngine(String? selectedEngine) async {
+    await flutterTts.setEngine(selectedEngine!);
+    language.value = null;
+    engine.value = selectedEngine;
+  }
+
+  List<DropdownMenuItem<String>> getLanguageOptions(List<dynamic> languages) {
+    var items = <DropdownMenuItem<String>>[];
+    for (dynamic type in languages) {
+      items.add(DropdownMenuItem(
+          value: type as String?, child: Text((type as String))));
+    }
+    return items;
+  }
+
+  void changeLanguage(String? language) {
+    this.language.value = language;
+    flutterTts.setLanguage(this.language.value!);
+    if (platformParams.android) {
+      flutterTts
+          .isLanguageInstalled(this.language.value!)
+          .then((value) => isCurrentLanguageInstalled = (value as bool));
+    }
+  }
+
+  Widget _buildEngineOptionWidget() {
+    if (platformParams.android) {
+      return FutureBuilder<dynamic>(
+          future: _getEngines(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasData) {
+              return _enginesDropDownSection(snapshot.data as List<dynamic>);
+            } else if (snapshot.hasError) {
+              return CommonAutoSizeText(
+                  AppLocalizations.t('Error loading engines...'));
+            } else {
+              return CommonAutoSizeText(
+                  AppLocalizations.t('Loading engines...'));
+            }
+          });
+    } else {
+      return const SizedBox(width: 0, height: 0);
+    }
+  }
+
+  Widget _buildLanguageOptionWidget() {
+    return FutureBuilder<dynamic>(
+        future: _getLanguages(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            return _buildLanguageOption(snapshot.data as List<dynamic>);
+          } else if (snapshot.hasError) {
+            return CommonAutoSizeText(
+                AppLocalizations.t('Error loading languages...'));
+          } else {
+            return CommonAutoSizeText(
+                AppLocalizations.t('Loading Languages...'));
+          }
+        });
+  }
+
+  Widget _buildBtnWidget() {
+    return Container(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: ButtonBar(
+        children: [
+          IconButton(
+            onPressed: () {
+              speak('');
+            },
+            icon: const Icon(Icons.play_arrow),
+            color: Colors.green,
+            splashColor: Colors.greenAccent,
+          ),
+          IconButton(
+            onPressed: stop,
+            icon: const Icon(
+              Icons.stop,
+            ),
+            color: Colors.red,
+            splashColor: Colors.redAccent,
+          ),
+          IconButton(
+            onPressed: pause,
+            icon: const Icon(
+              Icons.pause,
+            ),
+            color: Colors.blue,
+            splashColor: Colors.blueAccent,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _enginesDropDownSection(List<dynamic> engines) {
+    return Container(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: DropdownButton(
+          value: engine,
+          items: getEnginesDropDownMenuItems(engines),
+          onChanged: (Object? item) {
+            changeEngine(item?.toString());
+          }),
+    );
+  }
+
+  /// 选择语言
+  Widget _buildLanguageOption(List<dynamic> languages) {
+    return Container(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ValueListenableBuilder(
+            valueListenable: language,
+            builder: (BuildContext context, language, Widget? child) {
+              return DropdownButton(
+                  value: language,
+                  items: getLanguageOptions(languages),
+                  onChanged: (Object? item) {
+                    changeLanguage(item?.toString());
+                  });
+            },
+          ),
+          Visibility(
+            visible: platformParams.android,
+            child: CommonAutoSizeText(
+                "${AppLocalizations.t("Is installed")}: $isCurrentLanguageInstalled"),
+          ),
+        ]));
+  }
+
+  /// 音量
+  Widget _buildVolumeWidget() {
+    return ValueListenableBuilder(
+      valueListenable: volume,
+      builder: (BuildContext context, volume, Widget? child) {
+        return Slider(
+            value: volume,
+            onChanged: (newVolume) {
+              this.volume.value = newVolume;
+            },
+            min: 0.0,
+            max: 1.0,
+            divisions: 10,
+            label: "${AppLocalizations.t("Volume")}: $volume");
+      },
+    );
+  }
+
+  /// 音高
+  Widget _buildPitchWidget() {
+    return ValueListenableBuilder(
+        valueListenable: pitch,
+        builder: (BuildContext context, pitch, Widget? child) {
+          return Slider(
+            value: pitch,
+            onChanged: (newPitch) {
+              this.pitch.value = newPitch;
+            },
+            min: 0.5,
+            max: 2.0,
+            divisions: 15,
+            label: "${AppLocalizations.t("Pitch")}: $pitch",
+            activeColor: Colors.red,
+          );
+        });
+  }
+
+  /// 速度
+  Widget _buildRateWidget() {
+    return ValueListenableBuilder(
+        valueListenable: rate,
+        builder: (BuildContext context, rate, Widget? child) {
+          return Slider(
+            value: rate,
+            onChanged: (newRate) {
+              this.rate.value = newRate;
+            },
+            min: 0.0,
+            max: 1.0,
+            divisions: 10,
+            label: "${AppLocalizations.t("Rate")}: $rate",
+            activeColor: Colors.green,
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildBtnWidget(),
+        _buildEngineOptionWidget(),
+        _buildLanguageOptionWidget(),
+        Column(
+          children: [
+            _buildVolumeWidget(),
+            _buildPitchWidget(),
+            _buildRateWidget()
+          ],
+        ),
+      ],
+    );
+  }
+
+  void dispose() {
+    flutterTts.stop();
+  }
+}
