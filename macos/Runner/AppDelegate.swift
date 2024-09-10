6ec70e361240255517f32a269df9ed57c67880d1
hujs888 <hujs@colla.cc>
2024-09-10 21:39:48 +0800

    1.7.0 upgrade

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Cocoa
 import FlutterMacOS
 
+@NSApplication
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
