156d08d60ac328fdd98e977f2d7576156b67f0a1
hujs888 <hujs@colla.cc>
2024-11-01 02:26:40 +0800

    1.7.0 upgrade

--- lib/pages/game/model/component/line_component.dart
+++ lib/pages/game/model/component/line_component.dart
@@ -2,6 +2,7 @@ import 'package:colla_chat/pages/game/model/base/model_node.dart';
 import 'package:colla_chat/pages/game/model/base/node.dart';
 import 'package:colla_chat/pages/game/model/base/project.dart';
 import 'package:colla_chat/pages/game/model/component/model_flame_game.dart';
+import 'package:colla_chat/pages/game/model/component/node_position_component.dart';
 import 'package:colla_chat/pages/game/model/controller/model_project_controller.dart';
 import 'package:colla_chat/pages/game/model/widget/relationship_edit_widget.dart';
 import 'package:colla_chat/tool/dialog_util.dart';
@@ -40,9 +41,17 @@ class LineComponent extends PositionComponent
 
   @override
   void render(Canvas canvas) {
-    double srcX = nodeRelationship.src!.nodePositionComponent!.position.x;
-    double srcY = nodeRelationship.src!.nodePositionComponent!.position.y;
-    double srcHeight = nodeRelationship.src!.nodePositionComponent!.size.y;
+    if (nodeRelationship.src == null) {
+      return;
+    }
+    NodePositionComponent? srcNodePositionComponent =
+        nodeRelationship.src!.nodePositionComponent;
+    if (srcNodePositionComponent == null) {
+      return;
+    }
+    double srcX = srcNodePositionComponent.position.x;
+    double srcY = srcNodePositionComponent.position.y;
+    double srcHeight = srcNodePositionComponent.size.y;
     Vector2 srcTopCenter = Vector2(srcX + Project.nodeWidth / 2, srcY);
     Vector2 srcLeftCenter = Vector2(srcX, srcY + srcHeight / 2);
     Vector2 srcRightCenter =
@@ -50,32 +59,29 @@ class LineComponent extends PositionComponent
     Vector2 srcBottomCenter =
         Vector2(srcX + Project.nodeWidth / 2, srcY + srcHeight);
 
-    double dstX = nodeRelationship.dst!.nodePositionComponent!.position.x;
-    double dstY = nodeRelationship.dst!.nodePositionComponent!.position.y;
-    double dstHeight = nodeRelationship.dst!.nodePositionComponent!.size.y;
+    if (nodeRelationship.dst == null) {
+      return;
+    }
+    NodePositionComponent? dstNodePositionComponent =
+        nodeRelationship.dst!.nodePositionComponent;
+    if (dstNodePositionComponent == null) {
+      return;
+    }
+    double dstX = dstNodePositionComponent.position.x;
+    double dstY = dstNodePositionComponent.position.y;
+    double dstHeight = dstNodePositionComponent.size.y;
     Vector2 dstTopCenter = Vector2(dstX + Project.nodeWidth / 2, dstY);
     Vector2 dstLeftCenter = Vector2(dstX, dstY + srcHeight / 2);
     Vector2 dstRightCenter =
         Vector2(dstX + Project.nodeWidth, dstY + srcHeight / 2);
     Vector2 dstBottomCenter =
         Vector2(dstX + Project.nodeWidth / 2, dstY + srcHeight);
-    Offset from;
-    Offset to;
-    Path path = Path();
-    if (nodeRelationship.src!.nodePositionComponent!.modelNode ==
-        nodeRelationship.dst!.nodePositionComponent!.modelNode) {
-      from = Offset(srcBottomCenter.x, srcBottomCenter.y);
-      to = Offset(dstRightCenter.x, dstRightCenter.y);
-      path.moveTo(srcBottomCenter.x, srcBottomCenter.y);
-      path.lineTo(srcBottomCenter.x, srcBottomCenter.y + 30);
-      path.lineTo(
-          srcBottomCenter.x + Project.nodeWidth, srcBottomCenter.y + 30);
-      path.lineTo(srcBottomCenter.x + Project.nodeWidth, dstRightCenter.y);
-      path.lineTo(dstRightCenter.x, dstRightCenter.y);
 
-      path.lineTo(dstRightCenter.x + 12, dstRightCenter.y - 4);
-      path.moveTo(dstRightCenter.x, dstRightCenter.y);
-      path.lineTo(dstRightCenter.x + 12, dstRightCenter.y + 4);
+    Path path = Path();
+    if (srcNodePositionComponent.modelNode ==
+        dstNodePositionComponent.modelNode) {
+      selfLine(path, srcBottomCenter, srcRightCenter);
+      rightCenterArrow(path, srcRightCenter);
       canvas.drawPath(path, strokePaint);
 
       return;
@@ -86,65 +92,21 @@ class LineComponent extends PositionComponent
         // src在dst的上边
         if (srcY + srcHeight < dstY - 30) {
           // src在dst的全上边
-          from = Offset(srcBottomCenter.x, srcBottomCenter.y);
-          to = Offset(dstTopCenter.x, dstTopCenter.y);
-          path.moveTo(srcBottomCenter.x, srcBottomCenter.y);
-          path.lineTo(srcBottomCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x, dstTopCenter.y);
-
-          path.lineTo(dstTopCenter.x - 4, dstTopCenter.y - 12);
-          path.moveTo(dstTopCenter.x, dstTopCenter.y);
-          path.lineTo(dstTopCenter.x + 4, dstTopCenter.y - 12);
+          bottomTopLine(path, srcBottomCenter, dstTopCenter);
+          topCenterArrow(path, dstTopCenter);
         } else {
-          from = Offset(srcRightCenter.x, srcRightCenter.y);
-          to = Offset(dstLeftCenter.x, dstLeftCenter.y);
-          path.moveTo(srcRightCenter.x, srcRightCenter.y);
-          path.lineTo(
-              srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
-              srcRightCenter.y);
-          path.lineTo(
-              srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
-              dstLeftCenter.y);
-          path.lineTo(dstLeftCenter.x, dstLeftCenter.y);
-
-          path.lineTo(dstLeftCenter.x - 12, dstLeftCenter.y - 4);
-          path.moveTo(dstLeftCenter.x, dstLeftCenter.y);
-          path.lineTo(dstLeftCenter.x - 12, dstLeftCenter.y + 4);
+          rightLeftLine(path, srcRightCenter, dstLeftCenter);
+          leftCenterArrow(path, dstLeftCenter);
         }
       } else {
         // src在dst的下边
         if (dstY + dstHeight < srcY - 30) {
           // src在dst的全下边
-          from = Offset(srcTopCenter.x, srcTopCenter.y);
-          to = Offset(dstBottomCenter.x, dstBottomCenter.y);
-          path.moveTo(srcTopCenter.x, srcTopCenter.y);
-          path.lineTo(srcTopCenter.x,
-              srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
-          path.lineTo(dstBottomCenter.x,
-              srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
-          path.lineTo(dstBottomCenter.x, dstBottomCenter.y);
-
-          path.lineTo(dstBottomCenter.x - 4, dstBottomCenter.y + 12);
-          path.moveTo(dstBottomCenter.x, dstBottomCenter.y);
-          path.lineTo(dstBottomCenter.x + 4, dstBottomCenter.y + 12);
+          topBottomLine(path, srcTopCenter, dstBottomCenter);
+          bottomCenterArrow(path, dstBottomCenter);
         } else {
-          from = Offset(srcRightCenter.x, srcRightCenter.y);
-          to = Offset(dstLeftCenter.x, dstLeftCenter.y);
-          path.moveTo(srcRightCenter.x, srcRightCenter.y);
-          path.lineTo(
-              srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
-              srcRightCenter.y);
-          path.lineTo(
-              srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
-              dstLeftCenter.y);
-          path.lineTo(dstLeftCenter.x, dstLeftCenter.y);
-
-          path.lineTo(dstLeftCenter.x - 12, dstLeftCenter.y - 4);
-          path.moveTo(dstLeftCenter.x, dstLeftCenter.y);
-          path.lineTo(dstLeftCenter.x - 12, dstLeftCenter.y + 4);
+          rightLeftLine(path, srcRightCenter, dstLeftCenter);
+          leftCenterArrow(path, dstLeftCenter);
         }
       }
     } else if (dstX + Project.nodeWidth < srcX) {
@@ -153,65 +115,21 @@ class LineComponent extends PositionComponent
         // src在dst的上边
         if (srcY + srcHeight < dstY - 30) {
           // src在dst的全上边
-          from = Offset(srcBottomCenter.x, srcBottomCenter.y);
-          to = Offset(dstTopCenter.x, dstTopCenter.y);
-          path.moveTo(srcBottomCenter.x, srcBottomCenter.y);
-          path.lineTo(srcBottomCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x, dstTopCenter.y);
-
-          path.lineTo(dstTopCenter.x - 4, dstTopCenter.y - 12);
-          path.moveTo(dstTopCenter.x, dstTopCenter.y);
-          path.lineTo(dstTopCenter.x + 4, dstTopCenter.y - 12);
+          bottomTopLine(path, srcBottomCenter, dstTopCenter);
+          topCenterArrow(path, dstTopCenter);
         } else {
-          from = Offset(srcLeftCenter.x, srcLeftCenter.y);
-          to = Offset(dstRightCenter.x, dstRightCenter.y);
-          path.moveTo(srcLeftCenter.x, srcLeftCenter.y);
-          path.lineTo(
-              srcLeftCenter.x - (srcLeftCenter.x - dstRightCenter.x) / 2,
-              srcLeftCenter.y);
-          path.lineTo(
-              srcLeftCenter.x - (srcLeftCenter.x - dstRightCenter.x) / 2,
-              dstRightCenter.y);
-          path.lineTo(dstRightCenter.x, dstRightCenter.y);
-
-          path.lineTo(dstRightCenter.x + 12, dstRightCenter.y - 4);
-          path.moveTo(dstRightCenter.x, dstRightCenter.y);
-          path.lineTo(dstRightCenter.x + 12, dstRightCenter.y + 4);
+          leftRightLine(path, srcLeftCenter, dstRightCenter);
+          rightCenterArrow(path, dstRightCenter);
         }
       } else {
         // src在dst的下边
         if (dstY + dstHeight < srcY - 30) {
           // src在dst的全下边
-          from = Offset(srcTopCenter.x, srcTopCenter.y);
-          to = Offset(dstBottomCenter.x, dstBottomCenter.y);
-          path.moveTo(srcTopCenter.x, srcTopCenter.y);
-          path.lineTo(srcTopCenter.x,
-              srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
-          path.lineTo(dstBottomCenter.x,
-              srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
-          path.lineTo(dstBottomCenter.x, dstBottomCenter.y);
-
-          path.lineTo(dstBottomCenter.x - 4, dstBottomCenter.y + 12);
-          path.moveTo(dstBottomCenter.x, dstBottomCenter.y);
-          path.lineTo(dstBottomCenter.x + 4, dstBottomCenter.y + 12);
+          topBottomLine(path, srcTopCenter, dstBottomCenter);
+          bottomCenterArrow(path, dstBottomCenter);
         } else {
-          from = Offset(srcLeftCenter.x, srcLeftCenter.y);
-          to = Offset(dstRightCenter.x, dstRightCenter.y);
-          path.moveTo(srcLeftCenter.x, srcLeftCenter.y);
-          path.lineTo(
-              srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
-              srcRightCenter.y);
-          path.lineTo(
-              srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
-              dstLeftCenter.y);
-          path.lineTo(dstRightCenter.x, dstRightCenter.y);
-
-          path.lineTo(dstRightCenter.x + 12, dstRightCenter.y - 4);
-          path.moveTo(dstRightCenter.x, dstRightCenter.y);
-          path.lineTo(dstRightCenter.x + 12, dstRightCenter.y + 4);
+          leftRightLine(path, srcLeftCenter, dstRightCenter);
+          rightCenterArrow(path, dstRightCenter);
         }
       }
     } else {
@@ -220,62 +138,100 @@ class LineComponent extends PositionComponent
         // src在dst的上边
         if (srcY + srcHeight < dstY) {
           // src在dst的全上边
-          from = Offset(srcBottomCenter.x, srcBottomCenter.y);
-          to = Offset(dstTopCenter.x, dstTopCenter.y);
-          path.moveTo(srcBottomCenter.x, srcBottomCenter.y);
-          path.lineTo(srcBottomCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x, dstTopCenter.y);
-
-          path.lineTo(dstTopCenter.x - 4, dstTopCenter.y - 12);
-          path.moveTo(dstTopCenter.x, dstTopCenter.y);
-          path.lineTo(dstTopCenter.x + 4, dstTopCenter.y - 12);
+          bottomTopLine(path, srcBottomCenter, dstTopCenter);
+          topCenterArrow(path, dstTopCenter);
         } else {
-          from = Offset(srcBottomCenter.x, srcBottomCenter.y);
-          to = Offset(dstTopCenter.x, dstTopCenter.y);
-          path.moveTo(srcBottomCenter.x, srcBottomCenter.y);
-          path.lineTo(srcBottomCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x,
-              srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
-          path.lineTo(dstTopCenter.x, dstTopCenter.y);
-
-          path.lineTo(dstTopCenter.x - 4, dstTopCenter.y - 12);
-          path.moveTo(dstTopCenter.x, dstTopCenter.y);
-          path.lineTo(dstTopCenter.x + 4, dstTopCenter.y - 12);
+          bottomTopLine(path, srcBottomCenter, dstTopCenter);
+          topCenterArrow(path, dstTopCenter);
         }
       } else {
         // src在dst的下边
         if (dstY + dstHeight < srcY) {
           // src在dst的全下边
-          from = Offset(srcTopCenter.x, srcTopCenter.y);
-          to = Offset(dstBottomCenter.x, dstBottomCenter.y);
-          path.moveTo(srcTopCenter.x, srcTopCenter.y);
-          path.lineTo(srcTopCenter.x,
-              srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
-          path.lineTo(dstBottomCenter.x,
-              srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
-          path.lineTo(dstBottomCenter.x, dstBottomCenter.y);
-
-          path.lineTo(dstBottomCenter.x - 4, dstBottomCenter.y + 12);
-          path.moveTo(dstBottomCenter.x, dstBottomCenter.y);
-          path.lineTo(dstBottomCenter.x + 4, dstBottomCenter.y + 12);
+          topBottomLine(path, srcTopCenter, dstBottomCenter);
+          bottomCenterArrow(path, dstBottomCenter);
         } else {
-          from = Offset(srcLeftCenter.x, srcLeftCenter.y);
-          to = Offset(dstBottomCenter.x, dstBottomCenter.y);
-          path.moveTo(srcLeftCenter.x, srcLeftCenter.y);
-          path.lineTo(dstBottomCenter.x, srcLeftCenter.y);
-          path.lineTo(dstBottomCenter.x, dstBottomCenter.y);
-
-          path.lineTo(dstTopCenter.x - 4, dstTopCenter.y - 12);
-          path.moveTo(dstTopCenter.x, dstTopCenter.y);
-          path.lineTo(dstTopCenter.x + 4, dstTopCenter.y - 12);
+          leftBottomLine(path, srcLeftCenter, dstBottomCenter);
+          topCenterArrow(path, dstTopCenter);
         }
       }
     }
     canvas.drawPath(path, strokePaint);
+  }
+
+  void leftBottomLine(
+      Path path, Vector2 srcLeftCenter, Vector2 dstBottomCenter) {
+    path.moveTo(srcLeftCenter.x, srcLeftCenter.y);
+    path.lineTo(dstBottomCenter.x, srcLeftCenter.y);
+    path.lineTo(dstBottomCenter.x, dstBottomCenter.y);
+  }
+
+  void leftRightLine(Path path, Vector2 srcLeftCenter, Vector2 dstRightCenter) {
+    path.moveTo(srcLeftCenter.x, srcLeftCenter.y);
+    path.lineTo(srcLeftCenter.x - (srcLeftCenter.x - dstRightCenter.x) / 2,
+        srcLeftCenter.y);
+    path.lineTo(srcLeftCenter.x - (srcLeftCenter.x - dstRightCenter.x) / 2,
+        dstRightCenter.y);
+    path.lineTo(dstRightCenter.x, dstRightCenter.y);
+  }
+
+  void topBottomLine(Path path, Vector2 srcTopCenter, Vector2 dstBottomCenter) {
+    path.moveTo(srcTopCenter.x, srcTopCenter.y);
+    path.lineTo(srcTopCenter.x,
+        srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
+    path.lineTo(dstBottomCenter.x,
+        srcTopCenter.y - (srcTopCenter.y - dstBottomCenter.y) / 2);
+    path.lineTo(dstBottomCenter.x, dstBottomCenter.y);
+  }
+
+  void rightLeftLine(Path path, Vector2 srcRightCenter, Vector2 dstLeftCenter) {
+    path.moveTo(srcRightCenter.x, srcRightCenter.y);
+    path.lineTo(srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
+        srcRightCenter.y);
+    path.lineTo(srcRightCenter.x + (dstLeftCenter.x - srcRightCenter.x) / 2,
+        dstLeftCenter.y);
+    path.lineTo(dstLeftCenter.x, dstLeftCenter.y);
+  }
+
+  void bottomTopLine(Path path, Vector2 srcBottomCenter, Vector2 dstTopCenter) {
+    path.moveTo(srcBottomCenter.x, srcBottomCenter.y);
+    path.lineTo(srcBottomCenter.x,
+        srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
+    path.lineTo(dstTopCenter.x,
+        srcBottomCenter.y + (dstTopCenter.y - srcBottomCenter.y) / 2);
+    path.lineTo(dstTopCenter.x, dstTopCenter.y);
+  }
+
+  void selfLine(Path path, Vector2 srcBottomCenter, Vector2 srcRightCenter) {
+    path.moveTo(srcBottomCenter.x, srcBottomCenter.y);
+    path.lineTo(srcBottomCenter.x, srcBottomCenter.y + 30);
+    path.lineTo(srcBottomCenter.x + Project.nodeWidth, srcBottomCenter.y + 30);
+    path.lineTo(srcBottomCenter.x + Project.nodeWidth, srcRightCenter.y);
+    path.lineTo(srcRightCenter.x, srcRightCenter.y);
+  }
+
+  void bottomCenterArrow(Path path, Vector2 dstBottomCenter) {
+    path.lineTo(dstBottomCenter.x - 4, dstBottomCenter.y + 12);
+    path.moveTo(dstBottomCenter.x, dstBottomCenter.y);
+    path.lineTo(dstBottomCenter.x + 4, dstBottomCenter.y + 12);
+  }
+
+  void leftCenterArrow(Path path, Vector2 dstLeftCenter) {
+    path.lineTo(dstLeftCenter.x - 12, dstLeftCenter.y - 4);
+    path.moveTo(dstLeftCenter.x, dstLeftCenter.y);
+    path.lineTo(dstLeftCenter.x - 12, dstLeftCenter.y + 4);
+  }
+
+  void topCenterArrow(Path path, Vector2 dstTopCenter) {
+    path.lineTo(dstTopCenter.x - 4, dstTopCenter.y - 12);
+    path.moveTo(dstTopCenter.x, dstTopCenter.y);
+    path.lineTo(dstTopCenter.x + 4, dstTopCenter.y - 12);
+  }
+
+  void rightCenterArrow(Path path, Vector2 dstRightCenter) {
+    path.lineTo(dstRightCenter.x + 12, dstRightCenter.y - 4);
+    path.moveTo(dstRightCenter.x, dstRightCenter.y);
+    path.lineTo(dstRightCenter.x + 12, dstRightCenter.y + 4);
   }
 
   @override
